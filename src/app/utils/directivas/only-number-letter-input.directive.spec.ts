83996a4c6a2d6279761cebea455375c8d2be5512
GBValdez <gabrielbenjaminvaldezdeleon@gmail.com>
2024-12-18 17:12:07 -0600

    Borre el componente de seats-select

--- src/app/utils/directivas/only-number-letter-input.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OnlyNumberLetterInputDirective } from './only-number-letter-input.directive';
-
-describe('OnlyNumberLetterInputDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OnlyNumberLetterInputDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/utils/dr-error-inputs/dr-error-inputs.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DrErrorInputsDirective } from './dr-error-inputs.directive';
-
-describe('DrErrorInputsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DrErrorInputsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
